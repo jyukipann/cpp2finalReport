474a5fd978ab14303029db796631bb382e557f20
JUKI <jyukipann@yahoo.co.jp>
2021-07-27 12:22:22 +0900

    complate

--- main.cpp
+++ main.cpp
@@ -14,14 +14,66 @@ class pazzle_8{
 	vector<int> pazzle;
 	map<char,bool> Movable {{'w',false},{'a',false},{'s',false},{'d',false}};
 	int turn = 0;
+	int blank = 0;
 	public:
 	pazzle_8(int _n){
 		n = _n;
+		blank = n*n;
 		pazzle = vector<int>(n*n,0);
-		*pazzle.begin() = 0;
-		iota(pazzle.begin()+1, pazzle.end(), 1);
-		for(auto it = pazzle.begin(); it != pazzle.end(); ++it){
-			cout << *it << endl;
+		iota(pazzle.begin(), pazzle.end(), 1);
+		shaffle();
+	}
+
+	bool isSolvable(){
+		vector<int> payyle(pazzle.size());
+		copy(pazzle.begin(), pazzle.end(), payyle.begin());
+		int now = 1;
+		int swapcount = 0;
+		int tmp = 0;
+		for(int i = 0; i < pazzle.size(); i++){
+			if(now <= pazzle.size()){
+				auto result = find(pazzle.begin()+now-1, pazzle.end(), now);
+				//cout << *result << endl;
+				if(pazzle.begin()+now-1 != result){
+					swapcount+=1;
+					tmp = *result;
+					*result = *(pazzle.begin()+now-1);
+					*(pazzle.begin()+now-1) = tmp;
+				}
+				now+=1;
+			}
+			//cout << "a";
+		}
+		//show();
+		//cout << "swap : " << swapcount << endl;
+		copy(payyle.begin(), payyle.end(), pazzle.begin());
+		auto result = find(pazzle.begin(), pazzle.end(), blank);
+		int i = (distance(pazzle.begin(), result)/n );
+		//cout << i << endl;
+		int j = (distance(pazzle.begin(), result)%n);
+		//cout << j << endl;
+		//cout << "dist : " << 6-(i+j) << endl;
+		if((i+j)%2 == swapcount%2){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	void refinePazzle(){
+		auto result = find(pazzle.begin(), pazzle.end(), blank);
+		if(pazzle.begin() == result){
+			int tmp = *(result+1);
+			*(result+1) = *(result+2);
+			*(result+2) = tmp;
+		}else if(pazzle.end()-1 == result){
+			int tmp = *(result-1);
+			*(result-1) = *(result-2);
+			*(result-2) = tmp;
+		}else{
+			int tmp = *(result-1);
+			*(result-1) = *(result+1);
+			*(result+1) = tmp;
 		}
 	}
 
@@ -36,8 +88,8 @@ class pazzle_8{
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
 				
-				if(*(begin + (n*i+j)) == 0){
-					cout << "  â–¡ ";
+				if(*(begin + (n*i+j)) == blank){
+					cout << "  @ ";
 				}else{
 					cout << setfill(' ') << setw(3) << *(begin + (n*i+j)) << " ";
 				}
@@ -48,19 +100,19 @@ class pazzle_8{
 
 	bool move(char dir){
 		//cout << "move" << endl;
-		auto result = find(pazzle.begin(), pazzle.end(), 0);
+		auto result = find(pazzle.begin(), pazzle.end(), blank);
 		if(dir == 'w'){
 			*result = *(result-n);
-			*(result-n) = 0;
+			*(result-n) = blank;
 		} else if(dir == 'a'){
 			*result = *(result-1);
-			*(result-1) = 0;
+			*(result-1) = blank;
 		} else if(dir == 's'){
 			*result = *(result+n);
-			*(result+n) = 0;
+			*(result+n) = blank;
 		} else if(dir == 'd'){
 			*result = *(result+1);
-			*(result+1) = 0;
+			*(result+1) = blank;
 		}
 		turn++;
 		return true;
@@ -68,7 +120,7 @@ class pazzle_8{
 
 	bool renewMovableList(){
 		//cout << "renewMovableList" << endl;
-		auto result = find(pazzle.begin(), pazzle.end(), 0);
+		auto result = find(pazzle.begin(), pazzle.end(), blank);
 		Movable['w'] = false;
 		Movable['a'] = false;
 		Movable['s'] = false;
@@ -116,20 +168,32 @@ class pazzle_8{
 		//cout << "shaffle" << endl;
 		mt19937_64 engine(time(0));
 		shuffle(pazzle.begin(), pazzle.end(), engine);
+		if(!isSolvable()){
+			refinePazzle();
+		}
 	}
 };
 
 int main(){
-	pazzle_8 pazzle = pazzle_8(4);
-	pazzle.shaffle();
+	int n = 0;
+	cout << "n = ";
+	cin >> n;
+	if(n < 3){
+		n = 3;
+	}
+	pazzle_8 pazzle = pazzle_8(n);
+/* 	if(pazzle.isSolvable()){
+		cout << "solvable" << endl;
+	}else{
+		cout << "cant solve" << endl;
+	} */
 	while(!pazzle.isFinish()){
 		pazzle.show();
 		pazzle.renewMovableList();
-		cout << "'wâ†‘ aâ† sâ†“ dâ†’' : ";
+		cout << "wª a© s« d¨ : ";
 		pazzle.move(pazzle.getMoveDir());
 	}
 	pazzle.show();
 	cout << "finish" << endl;
-	cin.get();
 	return 0;
 }
